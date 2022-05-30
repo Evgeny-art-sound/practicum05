4b85a4d302f0e54c64078d30fa32696ece6218a8
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-05-30 14:28:47 +0500

    add example 3

---
+++ example03/Program.cs
@@ -0,0 +1,26 @@
+﻿// программа для копирования массива
+Console.Clear();
+Console.Write("Введите желаемую длину массива: ");
+int l = int.Parse(Console.ReadLine()??"");
+
+int[] array = new int[l];
+Random rnd = new Random();
+
+int[] array2 = new int[l];
+
+for (int i = 0; i < l; i++)
+	array[i] = rnd.Next(-1000, 1000);
+int count = 1;
+int count2 = 1;
+foreach (int a in array)
+{
+	Console.WriteLine($"{count} элемент массива = {a}");
+	count++;
+}
+for (int i = 0; i < l; i++)
+	array2[i] = array[i];
+foreach (int b in array2)
+{
+	Console.WriteLine($"{count2} элемент массива 2 = {b}");
+	count2++;
+}
